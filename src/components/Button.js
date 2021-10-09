974256c20ef4fc992cb5b4185a2ca941d53bcfb5
Jakub Turis <jakub@kontentino.com>
2021-10-09 15:05:40 +0200

    Init button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,29 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import style from './button/Button.module.scss';
+import classes from 'react-style-classes'
 
-const Button = () => {
-  return <div>Button</div>
+const Button = ({
+  buttonText,
+  variant,
+  className,
+}) => {
+  return (
+    <div className={classes(style.button, style[variant], className)}>
+      {buttonText}
+    </div>
+  )
+}
+
+Button.propTypes = {
+  buttonText: PropTypes.string.isRequired,
+  className: PropTypes.string,
+  variant: PropTypes.oneOf(['primary', 'secondary', 'danger'])
+}
+
+Button.defaultProps = {
+  className: null,
+  variant: 'primary',
 }
 
 export default Button
